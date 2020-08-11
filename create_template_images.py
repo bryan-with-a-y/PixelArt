66d60e9ee8f0427c673cdb6d8bf70b5149774b2b
bryan-with-a-y <bryan.klouda@gmail.com>
2020-08-10 22:59:30 -0400

    updates template creation functions

--- create_template_images.py
+++ create_template_images.py
@@ -1,35 +1,27 @@
 from PIL import Image
 
-
-def create():
-    region_xsize = 10
-    region_ysize = 10
+def create_10x10():
+    region_xsize = 10 #image pixels
+    region_ysize = 10 #image pixels
     xseps = region_xsize//2 + 1
     yseps = region_ysize//2 + 1
-    minor_thickness = 10
-    #major_thickness = 20
-    minor_color = (128,128,128)
-    #major_color = (128,0,0)
-    pixel_thickness = 50
+    minor_thickness = 10 #real pixels
+    pixel_thickness = 50 #real pixels
 
     xsize = pixel_thickness*10 + minor_thickness*xseps
     ysize = pixel_thickness*10 + minor_thickness*yseps
-    
     im = Image.new("RGB", (xsize, ysize), ("GRAY"))
     pix = im.load()
+
     for yi in range(minor_thickness, ysize, pixel_thickness*2+minor_thickness):
         for xi in range(minor_thickness, xsize, pixel_thickness*2+minor_thickness):
-            print(xsize, ysize, xi, yi)
+            print(xi, yi)
             for yii in range(pixel_thickness*2):
                 for xii in range(pixel_thickness*2):
                     pix[xi+xii, yi+yii] = (0,0,0)
-            """pix[xi                , yi                ] = (0,0,0)
-            pix[xi+pixel_thickness, yi                ] = (0,0,0)
-            pix[xi                , yi+pixel_thickness] = (0,0,0)
-            pix[xi+pixel_thickness, yi+pixel_thickness] = (0,0,0)"""
-    im.save("./assets/images/70x40_template.png")
+    im.save("./assets/images/10x10_template.png")
 
-def create2():
+def create_20x20():
     im = Image.open("./assets/images/10x10_template.png")
     xsize, ysize = im.size
     pix = im.load()
@@ -45,8 +37,9 @@ def create2():
                     new_pix[xi+xii*xsize, yi+yii*ysize] = old_pix
     new_im.save("./assets/images/20x20_template.png")
 
-def main():
-    create2()
+def create_template_images():
+    create_10x10()
+    create_20x20()
 
-main()
+create_template_images()
 
